65cd7f3809b53e9bc66de4cb04a9a5a8e985d1e7
Yugandher Akkunuru <yugandher.akkunuru@oracle.com>
2023-07-16 19:58:22 +0530

    crated sample1 branch

--- src/org/lauchcode/master.java
+++ src/org/lauchcode/master.java
@@ -4,7 +4,7 @@ public class master {
 
   public static void main(String[] args) {
     // write your code here
-    System.out.println("10th Line");
-    System.out.println("20th line");
+    System.out.println("First Line");
+    System.out.println("second line");
   }
 }
