a73cc7f51c4c2511108c13b10a2219c1967330bc
Yugandher Akkunuru <yugandher.akkunuru@oracle.com>
2023-07-16 20:02:15 +0530

    crated sample2 branch

--- src/org/lauchcode/Main.java
+++ src/org/lauchcode/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World");
-        System.out.println("Sample World");
+        System.out.println("Sample2 World");
     }
 }
